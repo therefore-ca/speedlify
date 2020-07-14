416d41fb42103c1d3e851d5f6df5c085a62ea457
Homer <sean.homer@therefore.ca>
2020-07-14 06:41:03 -0400

    * Testing RM performance builds

---
+++ _data/sites/rm.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: "Rocky Mountaineer", // optional, falls back to object key
+	description: "RM Performance Audits",
+	options: {
+		frequency: 60 * 23, // 24 hours
+		// if the sites don’t share assets on the same domain, we can reset
+		// chrome with each run instead of each site in every run (it’s faster)
+		// freshChrome: "run",
+		freshChrome: "site",
+	},
+	urls: [
+		"https://www.rockymountaineer.com/",
+	]
+};
